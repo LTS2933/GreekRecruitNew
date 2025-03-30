cf8908e33aef0e748ce16a98a6848cfb272eaa0b
LiamSmith <LiamSmith@LIAMSMITHCDIBTM>
2025-03-30 14:33:12 -0400

    Working on Add PNM functionality

---
+++ GreekRecruit/Controllers/AddPNMController.cs
@@ -0,0 +1,33 @@
+﻿using GreekRecruit.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreekRecruit.Controllers
+{
+    public class AddPNMController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SubmitPNM(PNM pnm)
+        {
+            var fname = pnm.pnm_fname;
+            var lname = pnm.pnm_lname;
+            if (fname == null || lname == null)
+            {
+                ViewData["FlashMessage"] = "PNM's name cannot be empty!";
+                return View("Index");
+            }
+
+            var email = pnm.pnm_email;
+            var phone = pnm.pnm_phone;
+            var gpa = pnm.pnm_gpa;
+            var major = pnm.pnm_major;
+            var schoolyear = pnm.pnm_schoolyear;
+
+            //NEED TO FINISH THIS
+            return View("Index");
+
+        }
+    }
+}
