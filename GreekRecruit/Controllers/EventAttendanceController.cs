f314953472cf5e128fcd99182250eecf21486d4f
LiamSmith <LiamSmith@LIAMSMITHCDIBTM>
2025-04-06 15:31:58 -0400

    Added attendance controller, still working on the functionality and will need to implement the view

---
+++ GreekRecruit/Controllers/EventAttendanceController.cs
@@ -0,0 +1,37 @@
+﻿using GreekRecruit.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GreekRecruit.Controllers
+{
+    public class EventAttendanceController : Controller
+    {
+        private readonly SqlDataContext _context;
+        public EventAttendanceController (SqlDataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            var username = User.Identity?.Name;
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.username == username);
+            if (user == null) return Unauthorized();
+            
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> SubmitAttendance(EventAttendance attendance)
+        {
+            var username = User.Identity?.Name;
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.username == username);
+            if (user == null) return Unauthorized();
+
+            return View(); //Gotta fix this
+        }
+    }
+}
